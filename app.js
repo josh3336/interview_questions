acf044e7dba79b187f257b0af3f1830dc96f5ccb
Josh Pagano <joshuapagano@gmail.com>
2013-09-19 10:39:57 -0700

    attempting to work with heroku

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'app')));
+app.use(express.static(path.join(__dirname, 'app/bower_components')));
 
 // development only
 if ('development' == app.get('env')) {
